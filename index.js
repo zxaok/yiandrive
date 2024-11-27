fd12515e971cde2038e2f6e13f16c3043163c35d
zxaok <106792481+zxaok@users.noreply.github.com>
2024-11-27 18:18:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ module.exports = async (req, res) => {
     // 获取最终重定向的 URL
     const finalUrl = response.url;
 
-    return res.status(200).json({ videoLink: finalUrl });
+    // 直接返回最终的 URL
+    return res.status(200).send(finalUrl);
   } catch (error) {
     console.error('Error fetching video link:', error);
-    return res.status(500).json({ error: error.message });
+    return res.status(500).send('Internal Server Error');
   }
 };
